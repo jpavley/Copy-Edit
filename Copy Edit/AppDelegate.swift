d5a7111e55d846b4f82b65d80f987310a3c8c71a
John Pavley <johnpavley-imac5k@Johns-iMac-3.local>
2016-11-26 23:09:56 -0500

    scissor emoji

--- Copy Edit/AppDelegate.swift
+++ Copy Edit/AppDelegate.swift
@@ -10,11 +10,15 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
+    
+    var copyEditStatusItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        copyEditStatusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        copyEditStatusItem?.title = "✂️"
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
