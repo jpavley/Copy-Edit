7bc70f0842edcf2780b31338711f24abc8b8d32e
John Pavley <johnpavley-imac5k@johns-imac-3.local>
2016-11-26 22:20:35 -0500

    Initial Commit

---
+++ Copy Edit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Copy Edit
+//
+//  Created by John Pavley on 11/26/16.
+//  Copyright © 2016 John Pavley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
