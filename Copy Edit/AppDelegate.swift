74fbc549cee6848846f348f3eb9fcbb2f3252b3f
John Pavley <johnpavley-imac5k@Johns-iMac-3.local>
2016-11-26 23:25:28 -0500

    basic structure

--- Copy Edit/AppDelegate.swift
+++ Copy Edit/AppDelegate.swift
@@ -19,10 +19,51 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         copyEditStatusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
         copyEditStatusItem?.title = "✂️"
         
+        let copyEditMenu = NSMenu()
+        
+        copyEditMenu.addItem(NSMenuItem(title: "Plain Text", action: #selector(AppDelegate.plainText), keyEquivalent: ""))
+        copyEditMenu.addItem(NSMenuItem.separator())
+        copyEditMenu.addItem(NSMenuItem(title: "Markdown Text", action: #selector(AppDelegate.plainText), keyEquivalent: ""))
+        copyEditMenu.addItem(NSMenuItem(title: "Markdown Link", action: #selector(AppDelegate.plainText), keyEquivalent: ""))
+        copyEditMenu.addItem(NSMenuItem.separator())
+        copyEditMenu.addItem(NSMenuItem(title: "HTML Text", action: #selector(AppDelegate.plainText), keyEquivalent: ""))
+        copyEditMenu.addItem(NSMenuItem(title: "HTML Link", action: #selector(AppDelegate.plainText), keyEquivalent: ""))
+        copyEditMenu.addItem(NSMenuItem(title: "HTML Escaped", action: #selector(AppDelegate.plainText), keyEquivalent: ""))
+        
+        copyEditStatusItem?.menu = copyEditMenu
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func plainText() {
+        
+    }
+    
+    func markdownText() {
+        
+    }
+    
+    func markdownLink() {
+        
+    }
+    
+    func htmlText() {
+        
+    }
+    
+    func htmlLink() {
+    
+    }
+    
+    func htmlEscaped() {
+        
+    }
+    
+    func quit() {
+        
     }
 
 
